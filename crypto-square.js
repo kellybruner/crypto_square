5ea5beecf4282bfe8bea7c54c4d9533b6903f40e
kellybruner <brunerka@dukes.jmu.edu>
2016-04-22 17:41:12 -0400

    Update crypto-square.js

---
+++ crypto-square.js
@@ -0,0 +1,42 @@
+var Crypto = function(text) {
+	this.text = text;
+};
+
+Crypto.prototype.normalizePlaintext = function() {
+	return this.text.replace(/[\W]/g, "").toLowerCase();
+};
+
+Crypto.prototype.size = function() {
+	var length = this.normalizePlaintext().length;
+	return Math.ceil(Math.sqrt(length));
+
+};
+
+Crypto.prototype.plaintextSegments = function() {
+	var segments = [];
+	npt = this.normalizePlaintext(),
+	size = this.size();
+
+	for (var i = 0; i < npt.length; i += size) {
+		segments.push(npt.substr(i, size));
+	}
+
+	return segments;
+
+};
+
+Crypto.prototype.ciphertext = function() {
+	var ct = "";
+		size = this.size(),
+		segs = this.plaintextSegments();
+
+		for (var i = 0; i < size; i += 1) {
+			for (var j = 0; j < segs.length; j += 1) {
+				ct += segs[j].charAt(i);
+		}
+}
+
+	return ct;
+};
+
+module.exports = Crypto;
